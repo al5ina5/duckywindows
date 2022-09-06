825247757dfdbf3b6be42ff142f46fab57903e82
Sebastian Alsina <alsinas@me.com>
2022-09-06 17:19:48 -0400

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,145 @@
+export default function IndexPage() {
+    return (
+        <>
+
+            <div className="fixed w-full bg-orange-400 text-white z-10 text-xs md:text-sm p-2">
+                <div className="max-w-5xl mx-auto text-center">
+                    <p><b>Call today:</b> (239) 888 0092</p>
+                </div>
+            </div>
+
+            <div className="bg-blue-500 text-white w-full h-auto px-6 py-24 md:py-48">
+                <div className="max-w-5xl mx-auto space-y-8">
+                    <img className="w-32 md:w-48" src="/img/duck.png" alt="" />
+                    <div className="space-y-4">
+                        <p className="text-4xl md:text-6xl font-bold">Window washing done right.</p>
+                        <p className="text-3xl md:text-5xl font-bold"> Now serving the <span className="bg-orange-400">Golden Gate Estates</span>.</p>
+                    </div>
+                    <p className="opacity-50 text-2xl">Using next-generation bladed technology, transform your homes windows into clear-cut ullamco est nisi anim consectetur aliqua fugiat ea Lorem pariatur enim laboris aute id. Exercitation sint ullamco dolor amet ipsum ea occaecat sit.</p>
+                </div>
+            </div>
+
+            <div className="bg-gray-100 p-6 pb-24 space-y-12">
+                <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
+                    <div className="bg-white shadow-xl rounded p-6 space-y-2">
+                        <p className="text-2xl font-extrabold text-blue-400">Fast</p>
+                        <p className="opacity-50 text-lg">Book your appointment in under 2 minutes and forget. Poof. Windows are clean. Like it was done by some ninjas.</p>
+                    </div>
+                    <div className="md:-mt-12">
+                        <div className="bg-white shadow-xl rounded p-6 space-y-2">
+                            <p className="text-2xl font-extrabold text-pink-400">Friendly</p>
+                            <p className="opacity-50 text-lg">Run by humans who live next-door to you, we strive to give you a pleasent and memorable experience.</p>
+                        </div>
+                    </div>
+                    <div className="bg-white shadow-xl rounded p-6 space-y-2">
+                        <p className="text-2xl font-extrabold text-indigo-400">Familiar</p>
+                        <p className="opacity-50 text-lg">Ducky Windows is a 100% family-owned company based in right at home — in the heart of the Golden Gate Estates.</p>
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-center">
+                    <div className="space-y-4">
+                        <div className="text-center">
+                            <p className="opacity-30 text-sm uppercase font-medium">Based in the Golden Gate Estates</p>
+                            <p className="opacity-50 uppercase font-bold">Naples, Bonita Springs, and Beyond</p>
+                        </div>
+                        <div className="text-center space-y-4">
+                            <a className="inline-block bg-orange-400 border-b-4 border-orange-600 shadow-xl text-orange-700 text-3xl font-medium px-8 py-4 rounded-2xl" href="#">Book now!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="max-w-4xl mx-auto space-y-12 md:space-y-24 p-6 py-24 md:py-48">
+
+                <div className="space-y-2 text-3xl md:text-4xl">
+                    <p className="text-center font-medium text-blue-400">Cleaning is what we do.</p>
+                    <p className="opacity-50 text-center font-medium text-blue-400">Let us show you, neighbor.</p>
+                </div>
+
+                <ul className="grid place-content-center sm:grid-cols-2 gap-8">
+                    <li className="flex">
+                        <div className="px-4 text-4xl md:text-5xl font-extralight text-blue-400 opacity-50">01.</div>
+                        <div>
+                            <div className="text-xl font-bold">Windows</div>
+                            <p className="max-w-xs py-2">We love numbers! We collect data and insights, analyze
+                                them then take the time to learn about your objectives, ask the right questions to understand your
+                                business.</p>
+                        </div>
+                    </li>
+                    <li className="flex">
+                        <div className="px-4 text-4xl md:text-5xl font-extralight text-blue-400 opacity-50">02.</div>
+                        <div>
+                            <div className="text-xl font-bold">Glass French Doors</div>
+                            <p className="max-w-xs py-2">Solutions are born from proper research, hard work, and
+                                strong strategy. We build data-driven roadmaps for every project to make pave the way to success.
+                            </p>
+                        </div>
+                    </li>
+                    <li className="flex">
+                        <div className="px-4 text-4xl md:text-5xl font-extralight text-blue-400 opacity-50">03.</div>
+                        <div>
+                            <div className="text-xl font-bold">Sliding Glass Doors</div>
+                            <p className="max-w-xs py-2">We design the right solution for your business. We
+                                craft beautiful, intuitive, and user-friendly interfaces that are easy to use and easy to maintain.
+                            </p>
+                        </div>
+                    </li>
+                    <li className="flex">
+                        <div className="px-4 text-4xl md:text-5xl font-extralight text-blue-400 opacity-50">04.</div>
+                        <div>
+                            <div className="text-xl font-bold">Fogged Glass</div>
+                            <p className="max-w-xs py-2">We build the right solution for your business. We
+                                craft beautiful, intuitive, and user-friendly interfaces that are easy to use and easy to maintain.
+                            </p>
+                        </div>
+                    </li>
+                    <li className="flex">
+                        <div className="px-4 text-4xl md:text-5xl font-extralight text-blue-400 opacity-50">05.</div>
+                        <div>
+                            <div className="text-xl font-bold">Deployment</div>
+                            <p className="max-w-xs py-2">We build the right solution for your business. We
+                                craft beautiful, intuitive, and user-friendly interfaces that are easy to use and easy to maintain.
+                            </p>
+                        </div>
+                    </li>
+                </ul>
+
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
+                    <div className="space-y-4">
+                        <div className="rounded-2xl overflow-hidden p-2 rainbow bg-orange-500">
+                            <img className="rounded-2xl" src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="" />
+                        </div>
+                        <div className="text-2xl font-medium">
+                            <p>Samuel Alsina</p>
+                            <p className="text-sm opacity-50">Founder, Window Professional</p>
+                        </div>
+                    </div>
+
+                    <div className="space-y-4">
+                        <img className="w-full rounded-2xl" src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="" />
+                        <div className="text-2xl font-medium">
+                            <p>Isabella Alsina</p>
+                            <p className="text-sm opacity-50">Dispatcher</p>
+                        </div>
+                    </div>
+
+                    <div className="space-y-4">
+                        <img className="w-full rounded-2xl" src="https://tailus.io/sources/blocks/classic/preview/images/woman1.jpg" alt="" />
+                        <div className="text-2xl font-medium">
+                            <p>Sebastian Alsina</p>
+                            <p className="text-sm opacity-50">Tech & Communications</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div className="p-6 md:p-12 text-xs md:text-2xl flex items-center">
+                <p className="flex-1 font-medium">🦆 Ducky Windows</p>
+                <p className="opacity-25 font-medium">Based in Naples, FL</p>
+            </div>
+        </>
+    )
+}
