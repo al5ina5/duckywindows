e319253b47011c121b127cf5ab722e447d504114
Sebastian Alsina <alsinas@me.com>
2023-06-05 13:08:20 -0400

    Added Meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
+import Meta from '../components/Meta'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Meta />
             <Component {...pageProps} />
         </>
     )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Meta />
                 <Head>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
                 </Head>
