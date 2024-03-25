66861d7a8ede4904f5ba4a586f32a375a710f4e9
Sebastian Alsina <alsinas@me.com>
2024-03-24 21:31:22 -0400

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
-import Head from 'next/head'
-import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import Meta from '../components/Meta'
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
